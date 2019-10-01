9b68f52c1b09a9c1eb03881dfc7165615aea7611
Kyle <kirvine87@gmail.com>
2019-10-01 18:13:00 +0100

    adds attraction visited list tests in visitor

--- src/test/java/people/VisitorTest.java
+++ src/test/java/people/VisitorTest.java
@@ -1,5 +1,7 @@
 package people;
 
+import attractions.Attraction;
+import attractions.RollerCoaster;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,10 +10,12 @@ import static org.junit.Assert.assertEquals;
 public class VisitorTest {
 
     Visitor visitor;
+    Attraction attraction;
 
     @Before
     public void before(){
         visitor = new Visitor(14, 1.2, 40.0);
+        attraction = new RollerCoaster("Spin", 50);
     }
 
     @Test
@@ -27,5 +31,16 @@ public class VisitorTest {
     @Test
     public void hasMoney() {
         assertEquals(40.0, visitor.getMoney(), 0.1);
+    }
+
+    @Test
+    public void hasAtractionVisitedList(){
+        assertEquals(0, visitor.getVisitedAttractions().size());
+    }
+
+    @Test
+    public void canAddAtractionToVisitedList(){
+        visitor.visitAttraction(attraction);
+        assertEquals(1, visitor.getVisitedAttractions().size());
     }
 }
